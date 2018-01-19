10a5498e68cd3ad370f34a49db40a6f5f6588136
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-01-19 08:12:23 -0500

    qwerghq

--- Lab 3.1/src/MainMethod.java
+++ Lab 3.1/src/MainMethod.java
@@ -4,7 +4,7 @@ public class MainMethod
 	public static void main(String [] args)
 	{
 		int [] test1 = {1,5,3,8,2,5,4,10};
-		double [] test2 = {8,4,6,2,10,52,87,46};
+		double [] test2 = {8,4,6,2,10,52.35, 52.564,87,46.234};
 		String [] test3 = {"wow", "omg", "you think this will work?", "nah man no way", "way to be positive", "well let's try it", "did this actually work? amazing.."};
 		ArrayMethods1.insertionSort(test1);
 		ArrayMethods1.selectionSort(test2);
